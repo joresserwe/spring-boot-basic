2157f8874a647086ce67b8747c144e6b84baa674
Eunbin <joresserwe12@gmail.com>
2023-01-28 18:16:10 +0900

    add decorator

---
+++ src/main/kotlin/me/joresserwe/helloboot/HelloDecorator.kt
@@ -0,0 +1,11 @@
+package me.joresserwe.helloboot
+
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Service
+
+@Service
+@Primary
+class HelloDecorator(private val helloService: HelloService) : HelloService {
+
+    override fun sayHello(name: String) = "*${helloService.sayHello(name)}*"
+}
--- src/test/kotlin/me/joresserwe/helloboot/HelloServiceTest.kt
+++ src/test/kotlin/me/joresserwe/helloboot/HelloServiceTest.kt
@@ -3,10 +3,16 @@ package me.joresserwe.helloboot
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class HelloServiceTest:FunSpec({
+class HelloServiceTest : FunSpec({
     test("helloServiceTest") {
         val helloService = SimpleHelloService()
         val ret = helloService.sayHello("Test")
         ret shouldBe "Hello Test"
+    }
+
+    test("helloDecorator") {
+        val decorator = HelloDecorator { it }
+        val ret = decorator.sayHello("hello Test")
+        ret shouldBe "*hello Test*"
     }
 })
